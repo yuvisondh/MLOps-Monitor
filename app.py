78e89fdc7fe43ce681e891640691073da727aa14
yuvisondh <yuvrajsondhs@gmail.com>
2026-03-08 19:17:57 -0600

    imported pickle to load the trained model

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
+import pickle
+import numpy as np
+
+
 
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
     return 'hi'
+@app.route('/bye/<name>')
+def bye(name):  
+     return f'bye {name}'
 if __name__ == '__main__':
     app.run(debug=True)
 
